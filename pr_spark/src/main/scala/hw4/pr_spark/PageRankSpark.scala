8217ac439b2278938be71f6e1e122e2705d3db45
dgrubis <dgrubis@10.0.0.133>
2019-10-31 23:31:47 -0400

    added main

--- pr_spark/src/main/scala/hw4/pr_spark/PageRankSpark.scala
+++ pr_spark/src/main/scala/hw4/pr_spark/PageRankSpark.scala
@@ -1,7 +1,22 @@
 package hw4.pr_spark
 
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.log4j.LogManager
+import org.apache.log4j.Level
+
 object PageRankSparkMain {
   
-  
+  def main(args: Array[String]) {
+    val logger: org.apache.log4j.Logger = LogManager.getRootLogger
+    if (args.length != 2) {
+      logger.error("Usage:\nRS_RMain <input dir> <output dir>")
+      System.exit(1)
+    }
+    
+    
 
+    
+    
+}
 }
